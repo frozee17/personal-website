d9839e1227bd5a8ab8f8dcb4c1713e3130dd39a8
jazoulay <jazoulay@umich.edu>
2019-05-07 15:34:11 -0400

    Finished preliminary version of the header toolbar. Still need to add effects to it, make it stick, and link buttons to different pages.

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,30 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import headerStyles from "./header.module.css"
+import { FaGlasses } from "react-icons/fa"
+//import { redirectTo } from "@reach/router";
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `0.5rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header className={headerStyles.header}>
+
+    <Link to="/">
+      <div className={headerStyles.logo}>
+          <FaGlasses size="2rem"/>
+      </div>
+    </Link>
+
+    <div className={headerStyles.links}>
+      <ul>
+        <li>About Me </li>
+        <li>Projects</li>
+        <li>Resume</li>
+        <li>Contact</li>
+      </ul>
     </div>
+
+
+
   </header>
 )
 
